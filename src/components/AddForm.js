4f501a1b0f32a3c491b067477fca52d8f42a529a
Christian <christiansmith2394@gmail.com>
2021-11-05 18:44:22 -0400

    Not working just yet

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,61 +1,57 @@
-import React, { useState } from 'react';
-
-const AddForm = (props) => {
-    const [state, setState] = useState({
-        name:"",
-        position:"",
-        nickname:"",
-        description:""
-    });
-
-    //remove when error state is added
-    const errorMessage = "";
-
-    const handleChange = e => {
-        setState({
-            ...state,
-            [e.target.name]:e.target.value
-        });
-    }
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (state.name === "" || state.position === "" || state.nickname === "") {
-            //add in error action
-        }
-    }
-
-    return(<section>
-        <h2>Add Smurf</h2>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label htmlFor="name">Name:</label><br/>
-                <input onChange={handleChange} value={state.name} name="name" id="name" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="position">Position:</label><br/>
-                <input onChange={handleChange} value={state.position} name="position" id="position" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="nickname">Nickname:</label><br/>
-                <input onChange={handleChange} value={state.nickname} name="nickname" id="nickname" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="description">Description:</label><br/>
-                <textarea onChange={handleChange} value={state.description} name="description" id="description" />
-            </div>
-            {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
-            }
-            <button>Submit Smurf</button>
-        </form>
-    </section>);
-}
-
-export default AddForm;
+import axios from 'axios';
+import { START_FETCH, SUCCESS_FETCH, FAIL_FETCH, ADD_SMURF } from '../actions';
 
 //Task List:
-//1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
-//2. Replace all instances of the errorMessage static variable with your error message state value. 
-//3. Within the handleSubmit function, replace the static assignment to errorMessage with a call to the setError action. Test that an error is displayed when this validation code fails.
-//4. Within the handleSubmit function, call your addSmurf action with the smurf name, position, nickname and summury passed as arguments. Test that a smurf is correctly added to when the form is submitted.
+//1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+//2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
+//3. Add a standard action that allows us to set the value of the error message slice of state.
+
+
+// export const START_FETCH = "START_FETCH";
+// export const SUCCESS_FETCH = "SUCCESS_FETCH";
+// export const FAIL_FETCH = "FAIL_FETCH";
+// export const ADD_SMURF = "ADD_SMURF";
+
+
+export const fetchSmurfs = () => dispatch => {
+    console.log('dispatched');
+
+    dispatch(startFetch());
+
+    axios.get("http://localhost:3333/smurfs")
+        .then(resp=> {
+            dispatch(successFetch(resp.data))
+        })
+        .catch(err => {
+            dispatch(failFetch(err))
+        });
+}
+
+
+export const startFetch = ()=> {
+    return ({type:START_FETCH});
+}
+
+export const successFetch = (smurf)=> {
+    console.log("smurf",smurf)
+    return ({type:SUCCESS_FETCH, payload:smurf});
+}
+
+export const failFetch = (error)=> {
+    return ({type:FAIL_FETCH, payload:error});
+}
+
+export const addSmurf = (item) => {
+
+    axios.post("http://localhost:3333/smurfs", item)
+        .then (resp =>{
+            console.log('post', resp);
+        })
+        .catch (resp =>{
+            console.log('fail:', resp)
+        })
+    return({type:ADD_SMURF, payload:item})
+
+
+}
+
--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from "./reducers";
+
+
 const { worker } = require('./mocks/browser');
 worker.start();
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
     rootElement
 );
+
 
 //Task List:
 //1. Add in all necessary components and libary methods.
